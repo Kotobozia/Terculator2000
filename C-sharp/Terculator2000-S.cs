b7343e80c0508954f1217ef244206e346e605b12
Kotobozia <Kotobozia@users.noreply.github.com>
2022-03-14 18:04:26 +0200

    Create Terculator2000-S.cs

---
+++ C-sharp/Terculator2000-S.cs
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+double calculation (double a, string calc, double b, double &c)          // calculation function
+{
+// +
+        if (calc == "+")
+            {
+                c = a + b;
+            }
+        else
+            {
+// -
+            if (calc == "-")
+            {
+                c = a - b;
+            }
+            else
+                {
+// *
+                if (calc == "*")
+                    {
+                        c = a * b;
+                    }
+                    else
+                        {
+// /
+                        if (calc == "/")
+                            {
+                                c = a / b;
+                            }
+                        }
+                }
+            }
+    return c;
+}
+
+
+void output (double a, string calc, double b, double c)  // output function 
+{
+    cout << "Hacking Pentagon computers to calculate... " << a << " " << calc << " " << b << " = | " << c << " |" << endl;
+    cout << endl;
+}
+
+
+int main()
+{
+    // variables declaration
+
+    double a, b, c;
+    string calc = "*";
+    string ex = "O";
+    
+    //if no exit, work
+    
+    while (ex != "Y")
+    {
+        cout << "Enter A" << endl;
+        cin >> a;
+        cout << "Enter action" << endl;
+        cin >> calc;
+        while (calc == "x")                             //if wished to exit
+        {                                   
+            cout << "Do You wish to exit? Y/N" << endl;
+            cin >> ex;
+            if ((ex == "Y") || (ex == "y"))
+            {
+                return 0;
+            }
+            else
+            {
+                if ((ex == "N") || (ex == "n"))         // if changed mind
+                {                        
+                    cout << endl;
+                    cout << "Enter the task again. Reenter A." << endl;
+                    cout << "Enter A" << endl;
+                    cin >> a;
+                    cout << "Enter action" << endl;
+                    cin >> calc;
+                }
+            }
+        }
+        cout << "Enter B" << endl;
+        cin >> b;
+
+// call for calculation func
+
+        calculation(a, calc, b, c);
+
+/// cout
+        output (a, calc, b, c);
+    }
+    return 0;
+}
